36b66073204bd7597bb922e0d5945836ef770f31
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-05-28 14:11:25 +0900

    insertメソッドの実装

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdminisratorRepository;
 
 /**
@@ -17,6 +18,12 @@ public class AdministratorService {
 	@Autowired
 	private AdminisratorRepository repository;
 	
-	
+	/**
+	 * 管理者情報を登録するメソッド
+	 * @param 管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		repository.insert(administrator);
+	}
 
 }
